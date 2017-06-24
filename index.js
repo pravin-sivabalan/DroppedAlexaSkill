f162c87f1f3c3b87f48e025133ad9d0776b21db6
Pravin Sivabalan <pravinsivabalan@gmail.com>
2017-06-24 16:28:13 -0500

    Add initial

---
+++ index.js
@@ -0,0 +1,78 @@
+var Alexa = require('alexa-sdk');
+var request = require('request');
+var lambdaConfig = require('./configs/lambda-config.json');
+
+const skillName = 'Dropped';
+
+var handlers = {
+    "DefaultIntent": function(){
+      request.get('https://rss.itunes.apple.com/api/v1/us/apple-music/new-music/5/explicit/json', (err, response, body) => {
+        if(err || response.statusCode != 200) return this.emit(':tell', 'I am having some issues connecting to external services');
+        var results = JSON.parse(body).feed.results;
+        var speechOutput = "";
+        for(result of results){
+          speechOutput += result.artistName + ' released a new ' + result.kind + ' '  + result.name + '. ';
+        }
+        return this.emit(':tell', speechOutput);
+      });
+    },
+
+    "GenreIntent": function(){
+      var genre = this.event.request.intent.slots.Genre.value.toLowerCase();
+      if(genre === 'hip hop') genre = 'hip-hop';
+      if(genre === 'rap') genre = 'hip-hop/rap';
+      request.get('https://rss.itunes.apple.com/api/v1/us/apple-music/new-music/100/explicit/json', (err, response, body) => {
+        if(err || response.statusCode != 200) return this.emit(':tell', 'I am having some issues connecting to external services');
+        var results = JSON.parse(body).feed.results;
+        var speechOutput = "";
+        var count = 0;
+        for(result of results){
+          if(count == 5) break;
+          if(genre.indexOf(result.primaryGenreName.toLowerCase()) !== -1){
+            speechOutput += result.artistName + ' released a new ' + result.kind + ' '  + result.name + '. ';
+            count++;
+          }
+        }
+        if(speechOutput === "") speechOutput = 'Sorry, I could not find any new music from this genre';
+        return this.emit(':tell', speechOutput);
+      });
+    },
+
+    "LimitIntent": function(){
+      var limit = this.event.request.intent.slots.Limit.value;
+      request.get('https://rss.itunes.apple.com/api/v1/us/apple-music/new-music/'+limit+'/explicit/json', (err, response, body) => {
+        if(err || response.statusCode != 200) return this.emit(':tell', 'I am having some issues connecting to external services');
+        var results = JSON.parse(body).feed.results;
+        var speechOutput = "";
+        for(result of results){
+          speechOutput += result.artistName + ' released a new ' + result.kind + ' '  + result.name + '. ';
+        }
+        return this.emit(':tell', speechOutput);
+      });
+    },
+
+    "AMAZON.HelpIntent": function(){
+        var speechOutput = "";
+        speechOutput += "Here are some things you can say: ";
+        speechOutput += "Alexa ask dropped what new music came out today. ";
+        speechOutput += "Alexa ask dropped 5 new song. ";
+        speechOutput += "Alexa ask dropped new hip hop songs. ";
+        speechOutput += "So how can I help?";
+        this.emit(':ask', speechOutput, speechOutput);
+    },
+
+    "AMAZON.StopIntent": function(){
+        this.emit(':tell', "Goodbye");
+    },
+
+    "AMAZON.CancelIntent": function(){
+        this.emit(':tell', "Goodbye");
+    }
+};
+
+exports.handler = function (event, context) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = lambdaConfig.appId;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
